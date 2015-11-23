5c78eba3aba9c4a08a90e2daf696a4fd19371e5b
Megyes Miklos <miklos.megyes@lensa.com>
2015-11-23 12:57:28 +0100

    first commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	void Awake() {
+		instance = this;
+	}
+
+	public bool isGamePlaying = false;
+	public GameObject welcomePanel;
+	public GameObject gamePanel;
+
+	void Start() {
+		gamePanel.SetActive (false);
+	}
+
+	public void PlayGame() {
+		isGamePlaying = true;
+		welcomePanel.SetActive (false);
+		gamePanel.SetActive (true);
+	}
+}
