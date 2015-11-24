09da8e9ddf33e710eae3c5b0f5e84d454bbcd4e1
Megyes Miklos <miklos.megyes@lensa.com>
2015-11-24 16:15:49 +0100

    physics part

---
+++ Assets/FPSDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FPSDisplay : MonoBehaviour {
+
+	public Text display;
+
+	float deltaTime = 0.0f;
+
+	void Start() {
+		
+		Application.targetFrameRate = 60;
+	}
+	
+	void Update() {
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+		float fps = 1.0f / deltaTime;
+		display.text = ((int)fps).ToString();
+	}
+}
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -13,6 +13,9 @@ public class GameManager : MonoBehaviour {
 	public GameObject welcomePanel;
 	public GameObject gamePanel;
 
+	public Transform circle;
+	public Transform square;
+
 	void Start() {
 		gamePanel.SetActive (false);
 	}
@@ -21,5 +24,28 @@ public class GameManager : MonoBehaviour {
 		isGamePlaying = true;
 		welcomePanel.SetActive (false);
 		gamePanel.SetActive (true);
+	}
+
+	void Update() {
+
+		bool isTouched = (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began);
+
+		if (isGamePlaying)
+		if (Input.GetMouseButtonDown (0) || isTouched) {
+			var pos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+			SpawnItem (pos.x, pos.y);
+		}                    
+	}
+
+	private void SpawnItem(float x, float y) {
+		bool isCircle = (Random.Range (0, 2) == 0);
+		Object obj = isCircle ? circle : square;
+		Transform inst = Instantiate(obj, new Vector3 (x, y, 0), Quaternion.identity) as Transform;
+		float scale = Random.Range (0.7f, 1.5f);
+		inst.localScale = new Vector3 (
+			scale,
+			isCircle ? scale : 2f - scale,
+			0
+		);
 	}
 }
